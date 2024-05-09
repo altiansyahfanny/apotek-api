5c9f8a6d53e6172d05dbf71a7bcd709cd4795073
Altiansyah Fanny <altiansyahf.nutech@gmail.com>
2024-05-09 15:06:00 +0700

    add prisma service

---
+++ src/common/common.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    WinstonModule.forRoot({
+      format: winston.format.json(),
+      level: 'debug',
+      transports: [new winston.transports.Console()],
+    }),
+  ],
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class CommonModule {}
